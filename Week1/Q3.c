bfade3aa52c544ee93660ea2b13c5ed6367c53ce
dixitrohit54 <92837093+dixitrohit54@users.noreply.github.com>
2021-12-03 16:03:22 +0530

    updating

--- Week1/Q3.c
+++ Week1/Q3.c
@@ -2,4 +2,32 @@
 #include<stdio.h>
 int main()
 {
+int a,b,c;
+printf("Enter three numbers\n");
+scanf("%d%d%d",&a,&b,&c);
+/*if(a>b)
+{
+    if(a>c)
+    {
+    printf("%d is max",a);
+    }
+    else
+    {
+        printf("%d is max",c);
+    }
+}
+else
+{
+    if(b>c)
+    {
+    printf("%d is max",b);
+    }
+    else
+    {
+        printf("%d is max",c);
+    }
+}*/
+//this qn can be done by above syntax or below syntax.
+a>b?(a>c?printf("%d is max",a):printf("%d is max",c)):(b>c?printf("%d is max",b):printf("%d is max",c));
+return 0;
 }
